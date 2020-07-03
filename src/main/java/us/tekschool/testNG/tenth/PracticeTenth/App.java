5f3fed3122756d5e04296b5982a33515e1b306fb
unknown <sadatsayedibrahim@gmail.com>
2020-07-03 11:42:49 -0700

    tenth

---
+++ src/main/java/us/tekschool/testNG/tenth/PracticeTenth/App.java
@@ -0,0 +1,13 @@
+package us.tekschool.testNG.tenth.PracticeTenth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
